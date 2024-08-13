6319b6937ceb35514e8d6d45c1f2c6213d25afbd
Allison <allisonabinger@gmail.com>
2024-08-13 15:31:05 -0500

    A - 08/13 03:30 PM - Added swagger documenation with UI to /api-docs. Fully functional and organized.

--- client/hero-of-flavor/src/server/server.js
+++ client/hero-of-flavor/src/server/server.js
@@ -1,18 +1,51 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors')
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 const PORT = 5000;
 const app = express();
+
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use(express.json());
 app.use('/', routes);
 
+
+// swagger set up
+const options = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: "Hero of Flavor: Tears of the Kingdom API",
+            version: "1.0.0",
+            description: "An API to search for all ingredients, recipes, and a recipe tool for The Legend of Zelda: Tears of the Kingdom. Use the tools to make your cooking in TOTK much more efficient!"
+        },
+        servers: [
+            {
+                url: "http://localhost:5000"
+            }
+        ],
+    },
+    apis: ["./routes/*.js"]
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
+
+// // Serving Swagger docs as JSON
+// app.get('/swagger.json', (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.json(swaggerDocs);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
